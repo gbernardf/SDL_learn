2d32c4a99f20a342c97685a8d9363d3c6e7a3ea3
gui <g.bernardfloyd@gmail.com>
2014-11-04 10:34:53 -0500

    Added event handler that wait for the X-out of the window to close program. Added applyImage function to apply image on screen location

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 400;
+const string IMG_PATH = "../FILES/imgs/";
 
 int main(int argc, char* argv[]) {
 
@@ -15,30 +16,39 @@ int main(int argc, char* argv[]) {
     //the surface contained by the window
     SDL_Surface* screenSurface = NULL;
     SDL_Surface* background = NULL;
+    SDL_Surface* player = NULL;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Init error ! SDL_Error: %s\n", SDL_GetError());
     } else {
-
+        SDL_Event event;
         //create window
-        window = toolbox.createWindow();
+        window = toolbox.createWindow("SDL Game learn");
         if (window == NULL) {
             printf("Window could not be created! SDL_Error:%s\n", SDL_GetError());
         } else {
 
-            string path = "../FILES/imgs/back.bmp";
-            background = SDL_LoadBMP(path.c_str());
+            string backPath = IMG_PATH + "back.bmp";
+            string playerPath = IMG_PATH + "_carreGob.bmp";
 
-            background = toolbox.loadImage(path);
+            //background = SDL_LoadBMP(path.c_str());
+            player = toolbox.loadImage(playerPath);
+            background = toolbox.loadImage(backPath);
             if(background == NULL)
                 std::cout<<"SDL_LoadBMP failed." <<std::endl;
             //get window surface
             screenSurface = SDL_GetWindowSurface(window);
-            SDL_BlitSurface(background, NULL, screenSurface, NULL);
+            toolbox.applyImage(background,screenSurface,0,0);
+            toolbox.applyImage(player,screenSurface,304,160);
 
             SDL_UpdateWindowSurface(window);
-
-            SDL_Delay(2000);
+            bool run = true;
+            do{
+                while(SDL_PollEvent(&event)){
+                    if(event.type == SDL_QUIT)
+                        run = false;
+                }
+            }while(run);
         }
 
     }
